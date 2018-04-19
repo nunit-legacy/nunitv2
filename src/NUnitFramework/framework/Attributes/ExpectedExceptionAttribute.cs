01787c482162c2501b9eb16feae01b2f464f536c
Charlie Poole <charliepoole@gmail.com>
2018-04-19 15:14:54 -0700

    Additional updates for release

--- src/NUnitFramework/framework/Attributes/ExpectedExceptionAttribute.cs
+++ src/NUnitFramework/framework/Attributes/ExpectedExceptionAttribute.cs
@@ -1,5 +1,5 @@
 // ****************************************************************
-// Copyright 2018, Charlie Poole.
+// Copyright 2002-2018, Charlie Poole.
 // This is free software licensed under the NUnit license, a copy
 // of which should be included with this software. If not, you may
 // obtain a copy at https://github.com/nunit-legacy/nunitv2.
